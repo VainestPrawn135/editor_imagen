e27400694475dbbcff78c74a355b6601791bba83
Dave Moreno <roackdave@gmail.com>
2019-08-11 20:44:18 -0500

    Cambios en nombres de variables

---
+++ 20julio/editor/obtener.py
@@ -0,0 +1,16 @@
+from PIL import Image
+
+img = Image.open("../logoconfondo/blastfury-rojo.png")
+#convertimos a escala de RGBA
+if img.mode != 'RGBA':
+    img = img.convert('RGBA')
+datos = list(img.getdata())  # obtenemos los códigos de colores de cada píxel
+#print(datos)
+archivo = open("datos.txt", "w")
+archivo.write(" ".join(datos.__str__())+"\n")
+archivo.close()
+
+color = list(img.getbands())
+print(color)
+
+img.show()
---
+++ 20julio/editor/pixeles.py
@@ -0,0 +1,23 @@
+from PIL import Image
+
+img = Image.open("editor_imagen/20julio/editor/logosinfondo/blastfury-amarillo.png")
+#convertimos a escala de RGB, RGBA, L, HSV
+if img.mode != 'RGBA':
+    img = img.convert('RGBA')
+
+#print(datos)
+width = img.size[0]
+height = img.size[1]
+
+pixel = img.load()
+
+archivo = open("pixeles.txt", "w")
+
+#obtenemos todos los demás colores que no son los del fondo de la imagen
+for x in range(0, width):
+    for y in range(0, height):
+        if pixel[x, y] != (0, 0, 0, 255) and pixel[x, y] != (0, 0, 0, 0):
+            archivo.write(" ".join(pixel[x, y].__str__()))
+
+archivo.close()
+
